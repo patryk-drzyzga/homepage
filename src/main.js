e9592d61a03167e1011cef05bf5960f8630d12c5
Patryk Drzyzga <patryk.drzyzga98@gmail.com>
2020-05-22 22:01:38 +0200

    formatting main & style

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ var name = "Patryk";
 var age = "22";
 
 console.log(`Cześć, mam na imię ${name} i mam ${age} lata.`);
-const header = document.querySelector('.hero-header__text--js');
+const header = document.querySelector(".hero-header__text--js");
 console.log(header);
-header.innerHTML = 'Witaj na mojej stronie domowej!';
+header.innerHTML = "Witaj na mojej stronie domowej!";
